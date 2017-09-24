53979eb980f2bfaeca574a1231f3c059aaa2428b
Eduard Labella <labellaeduard@gmail.com>
2017-09-24 18:20:08 -0300

    Schemas Mongoose

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,11 +1,28 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/fotos",{
+  useMongoClient: true,
+
+});
 
 
+var useSchemaJSON = {
+  email:String,
+  password:String
+};
 
-app.use("/estatico",express.static('public'));
-app.use(express.static('assets'));
+
+var user_schema = new Schema(useSchemaJSON);
+
+var User = mongoose.model("User",user_schema);
+
+app.use("/public",express.static('public'));
+app.use(bodyParser.json()); //peticiones application/json
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "jade");
 
@@ -17,4 +34,10 @@ app.get("/login",function(req,res){
   res.render("login");
 });
 
+app.post("/users",function(req,res){
+  var user = new User({email: req.body.email, password: req.body.password});
+  user.save(function(){
+      res.send("Guardamos tus datos");
+  });
+});
 app.listen(8080);
